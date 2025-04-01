980c239d18fad53e7448b394fa121ed81be2511e
solserj <fgump259@gmail.com>
2025-04-01 10:52:20 +0300

    endOfLesson#4Part1 repeat

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,33 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import * as mongoose from "mongoose";
 import {config} from "./configs/configs";
 import {ApiRouter} from "./routers/api.router";
+import {ApiError} from "./errors/api.error";
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/", ApiRouter);
 
+app.use("*",
+    (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+       const status = err.status || 500;
+       const message = err.message ?? "Something went wrong";
+       res.status(status).json({status, message});
+    }
+);
+process.on("uncaughtException", (err: ApiError) => {//для непередбачених помилок
+    console.log("uncaughtException",err);
+    process.exit(1);
+});
+
 const dbConnection = async () => {
     try {
         let dbConnect = false;
         while (!dbConnect) {
             console.log("Database Connecting...");
-          await  mongoose.connect(config.MONGO_URI);
+            await mongoose.connect(config.MONGO_URI);
             dbConnect = true;
             console.log("Database Connected...");
         }
@@ -28,8 +42,9 @@ const start = async () => {
     try {
         await dbConnection();
         console.log("Database available");
-        app.listen(config.PORT, ()=>{
-            console.log(`Server started on port ${config.PORT}`);        })
+        app.listen(config.PORT, () => {
+            console.log(`Server started on port ${config.PORT}`);
+        })
     } catch (e) {
         console.log('Something went wrong', e.message)
     }
